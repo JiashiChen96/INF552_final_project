a3c06fb22b0267e577543d50a342a27e5421fab7
jasonchen96 <397390540@qq.com>
2020-05-06 16:47:35 -0700

    update data processing

--- project.py
+++ project.py
@@ -1,41 +1,43 @@
 import numpy as np
 import pandas as pd
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 class project_framework(object):
     def __init__(self, data):
         self.data = data.copy()
-    def clean_1(self):
-
-        #Drop the outliers
-        self.data.drop(self.data[self.data['hoa (R$)']==1117000].index, inplace=True)
-        self.data.drop(self.data[self.data['property tax (R$)']==313700].index, inplace=True)
-        self.data.drop(self.data[self.data['total (R$)']==1120000].index, inplace=True)
-        self.data.drop(self.data[self.data['floor']=='301'].index, inplace=True)
-
+        
+    def clean(self):
+        
         #Log transformation
-        self.data['hoa (R$)'] = self.data['hoa (R$)'] .apply(lambda x: np.log1p(x))
-        self.data['rent amount (R$)'] = self.data['rent amount (R$)'] .apply(lambda x: np.log1p(x))
-        self.data['property tax (R$)'] = self.data['property tax (R$)'].apply(lambda x: np.log1p(x))
-        self.data['fire insurance (R$)'] = self.data['fire insurance (R$)'].apply(lambda x: np.log1p(x))
-        self.data['total (R$)'] = self.data['total (R$)'].apply(lambda x: np.log1p(x))
+        log_attributes = ['hoa (R$)', 'rent amount (R$)', 'property tax (R$)', 'fire insurance (R$)', 'total (R$)']
+        for attr in log_attributes:
+            self.data[attr] = self.data[attr] .apply(lambda x: np.log1p(x))
 
         #Fill the missing values
         self.data['floor'] = self.data['floor'].apply(lambda x: replace_with_nan(x, '-'))
         self.data['floor'] = self.data['floor'].astype(float)
-        floor_mean = self.data['floor'].mean()
-        self.data['floor'] = self.data['floor'].apply(lambda x: fill_with_num(x, floor_mean))
-
-        # count = 0
-        # total = 0
-        # for floor in self.data["floor"]:
-        #     if floor != '-':
-        #         if floor != "301":
-        #             total += int(floor)
-        #         else:
-        #             total += 31
-        #         count += 1
-        # mean = total / count
-        # self.data['floor'] = self.data['floor'].apply(lambda x: fillFloor(x, mean))
+        self.data['floor'].fillna(self.data['floor'].mean(), inplace = True)
+        
+        missing_attributes = ["property tax (R$)", "hoa (R$)"]
+        for attr in missing_attributes:
+            self.data[attr] = self.data[attr].apply(lambda x: replace_with_nan(x, 0))
+        imp_mean = IterativeImputer(random_state=0)
+        imp_mean.fit(self.data[missing_attributes])
+        new_tax_hoa = imp_mean.transform(self.data[missing_attributes])
+        self.data["property tax (R$)"] = new_tax_hoa[:, 0]
+        self.data["hoa (R$)"] = new_tax_hoa[:, 1]
+        
+        #Drop the outliers
+        outliers_attributes = ['hoa (R$)', 'property tax (R$)', 'total (R$)']
+        
+        for attr in outliers_attributes:
+            mean = self.data[attr].mean()
+            std = self.data[attr].std()
+            self.data.drop(self.data[self.data[attr] - mean > 3 * std].index, inplace=True)
+            self.data.drop(self.data[self.data[attr] - mean < -3 * std].index, inplace=True)
+        
+        self.data.drop(self.data[self.data['floor']==301].index, inplace=True)
 
         #Transfor categorical attribute
         self.data['animal'] = self.data['animal'].apply(lambda x: fillAnimalFurniture(x))
@@ -44,10 +46,7 @@ class project_framework(object):
         onehot = pd.get_dummies(self.data['city'], drop_first = True)
         for colname in onehot:
             self.data[colname] = onehot[colname]
-        # self.data.drop(columns = ['city'], inplace = True)
 
-    def clean_2(slef):
-        self.data.drop(columns = ['hoa (R$)'], inplace = True)
 
 def replace_with_nan(x, old_val):
     return np.nan if x == old_val else x
@@ -55,21 +54,8 @@ def replace_with_nan(x, old_val):
 def fill_with_num(x, new_val):
     return new_val if np.isnan(x) else x
 
-
-
-def fillFloor(x, mean):
-    if x.isnumeric():
-        if x != "301":
-            return float(x)
-        else:
-            return 31.0
-    else:
-        return mean
-
 def fillAnimalFurniture(x):
     if x == 'acept' or x == 'furnished':
         return 1
     else:
         return 0
-## pro = project_framework(data)
-## pro.clean()
